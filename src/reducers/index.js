874a31a3619c9c78da76581384bb6ac4c69ab648
nisssy <kazuhiro.nishikawa.96@gmail.com>
2021-01-17 12:10:54 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,19 @@
+//state = [{id: 1, title: a, body: b}]
+
+const events = (state = [], action) => {
+    switch(action.type) {
+        case 'CREATE_EVENT':
+            const event = { title: action.title, body: action.body}
+            const length = state.length
+            const id = length === 0? 1 : state[length-1] + 1
+            return [...state, {id, ...event}]
+        case 'DELETE_EVENT':
+            return state
+        case 'DELETE_ALL_EVENT':
+            return []
+        default:
+            return state
+    }
+}
+
+export default events
